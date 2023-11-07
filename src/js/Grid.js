0f59f940c7427b831d3e6c2bffdc7a6f2af6eed2
Christopher Quattrocchi <chrisquattrocchi@gmail.com>
2023-11-07 14:08:59 -0800

    test pass for duplicate checker

--- src/js/Grid.js
+++ src/js/Grid.js
@@ -1,20 +1,4 @@
-// export default function Triangle(side1, side2, side3) {
-//   this.side1 = side1;
-//   this.side2 = side2;
-//   this.side3 = side3;
-// }
 
-// Triangle.prototype.checkType = function () {
-//   if (this.side1 > this.side2 + this.side3 || this.side2 > this.side1 + this.side3 || this.side3 > this.side1 + this.side2) {
-//     return "not a triangle";
-//   } else if (this.side1 !== this.side2 && this.side1 !== this.side3 && this.side2 !== this.side3) {
-//     return "scalene triangle";
-//   } else if (this.side1 === this.side2 && this.side1 === this.side3) {
-//     return "equilateral triangle";
-//   } else {
-//     return "isosceles triangle";
-//   }
-// };
 
 export default function Grid(rows, columns) {
   this.rows = rows;
@@ -37,6 +21,15 @@ Grid.prototype.assignNumbers = function() {
     }
   }
 }
+
+Grid.prototype.duplicateChecker = function() {
+  let set = new Set(grid.array[0]);
+  return set.size !== grid.array[0];
+}
+
+let grid = new Grid(9, 9);
+grid.create2dArray();
+grid.assignNumbers();
 
 // Grid.prototype.assignNumbers = function() {
 //   for (let i = 0; i < this.rows; i++) {
